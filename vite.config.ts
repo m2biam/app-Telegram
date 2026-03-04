438a70aa5194c43dde5851515ed316d7de69bc81
Marília Moreira <37786291+m2biam@users.noreply.github.com>
2026-03-04 12:43:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dyad from 'dyad'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dyad.dyadComponentTagger()
-  ],
+  plugins: [react()],
   base: './',
 })
